b0448147ec2b39decfa2d6a8b854e37f189466b2
Elliot Pettingale <elliot@smartercoding.co.uk>
2022-03-17 07:59:43 +0000

    setup websocket connection

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Echo from "laravel-echo";
+
+window.Pusher = require("pusher-js");
+
+window.Echo = new Echo({
+  broadcaster: "pusher",
+  key: "12345",
+  forceTLS: false,
+  wsHost: "localhost",
+  wsPort: 8002,
+  encrypted: false,
+});
 
 ReactDOM.render(
   <React.StrictMode>
