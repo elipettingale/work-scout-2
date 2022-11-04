23837313804583e87237523cb90f54dff05cdd24
Elliot Pettingale <elliot@smartercoding.co.uk>
2022-11-04 11:47:16 +0000

    greatly improve keyword highlighting

--- frontend/src/helpers/text.js
+++ frontend/src/helpers/text.js
@@ -1,19 +1,16 @@
 import keywords from "../config/keywords.json";
 
 export function highlightKeywords(html) {
-  keywords.good.forEach((keyword) => {
-    const regex = new RegExp(`(${keyword})`, "gi");
-    html = html.replaceAll(regex, '<span class="keyword is-good">$1</span>');
-  });
+  console.log(keywords);
 
-  keywords.bad.forEach((keyword) => {
-    const regex = new RegExp(`(${keyword})`, "gi");
-    html = html.replaceAll(regex, '<span class="keyword is-bad">$1</span>');
-  });
-
-  keywords.neutral.forEach((keyword) => {
-    const regex = new RegExp(`(${keyword})`, "gi");
-    html = html.replaceAll(regex, '<span class="keyword">$1</span>');
+  keywords.forEach((set) => {
+    set.words.forEach((word) => {
+      const regex = new RegExp(`(${word})`, "gi");
+      html = html.replaceAll(
+        regex,
+        `<span class="keyword is-${set.key}">$1</span>`
+      );
+    });
   });
 
   return html;
